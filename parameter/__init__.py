d81333c08821404d4ef1bd9ac9c1177b46bfe5cf
dl-box-g2 <gengshen.research@gmail.com>
2020-08-17 14:51:22 +0900

    added todo plan for updating

---
+++ parameter/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Created by Hai-Tao Yu | 2020/08/17 | https://y-research.github.io
+
+"""Description
+todo add DataSetting, ModelParameter, etc.
+"""
